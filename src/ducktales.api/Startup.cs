527341ff9aeb0fd095860df16f69d8471cf5921b
Andre Castro Garcia <andre.garcia@iteris.com.br>
2018-06-04 23:22:04 -0300

    ajuste na resposta e tempo de downtime em atualizações

--- src/ducktales.api/Startup.cs
+++ src/ducktales.api/Startup.cs
@@ -7,7 +7,7 @@ namespace ducktales.api {
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-            app.Run(async context => { await context.Response.WriteAsync("Hello World Ducktales"); });
+            app.Run(async context => { await context.Response.WriteAsync("Hello World Ducktales!"); });
         }
     }
 }
